e27a1882895bcefb2fd4d540b47f93280c2fb0aa
Xander Bailey <zander181@gmail.com>
2020-11-18 22:59:21 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="planter",
+    version="0.0.1",
+    author="Xander Bailey",
+    description="Raspberry Pi data logger host",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
