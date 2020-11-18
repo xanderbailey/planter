aed1d356ba1133d2ec05a795b65bb94ff5a08509
Xander Bailey <zander181@gmail.com>
2020-11-18 23:13:30 +0000

    Black

--- api/__init__.py
+++ api/__init__.py
@@ -88,27 +88,35 @@ def create_app(test_config=None):
 
     return app
 
-class Client():
-    def __init__(self, apikey:str, root_url:str):
+
+class Client:
+    def __init__(self, apikey: str, root_url: str):
         self.apikey = apikey
-        self.header = {"ApiKey":self.apikey}
+        self.header = {"ApiKey": self.apikey}
         self.url = root_url
 
-    def add_sensor(self, name:str, location:str):
-        data = {"name":name, "location":location}
+    def add_sensor(self, name: str, location: str):
+        data = {"name": name, "location": location}
         response = requests.post(self.url + "/sensors", data=data, headers=self.header)
         return response.json()
 
-    def add_reading(self, sensor_id:str, temperature:str, humidity:str, pressure:str):
-        data = {"sensor_id":sensor_id, "temperature":temperature, "humidity":humidity, "pressure":pressure}
+    def add_reading(
+        self, sensor_id: str, temperature: str, humidity: str, pressure: str
+    ):
+        data = {
+            "sensor_id": sensor_id,
+            "temperature": temperature,
+            "humidity": humidity,
+            "pressure": pressure,
+        }
         response = requests.post(self.url + "/readings", data=data, headers=self.header)
         return response.json()
-    
+
     def get_sensors(self):
         response = requests.get(self.url + "/sensors", headers=self.header)
         return response.json()
-    
-    def get_readings(self, from_date:str=None, to_date:str=None):
-        data = {"from":from_date, "to":to_date}
+
+    def get_readings(self, from_date: str = None, to_date: str = None):
+        data = {"from": from_date, "to": to_date}
         response = requests.get(self.url + "/readings", data=data, headers=self.header)
         return response.json()
